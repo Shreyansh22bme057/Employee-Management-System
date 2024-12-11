10683ccce399dc3970ca2d1a2f071b0c23b4e5eb
Shreyansh Tripathi <142929361+Shreyansh22bme057@users.noreply.github.com>
2024-12-12 02:22:36 +0530

    Add files via upload

---
+++ employee_ms/src/main.jsx
@@ -0,0 +1,31 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import ReactDOM from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import AuthProvider from './context/AuthProvider.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//       <AuthProvider>
+//         <App/>  
+//       </AuthProvider> 
+//   </StrictMode>
+// )
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import AuthProvider from './context/AuthProvider.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+
+  </React.StrictMode>,
+)
